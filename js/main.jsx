1528bfb653a1d5d6e90bf4ecadb826a07eed0113
vignesh-m <vicky.mano@gmail.com>
2016-01-05 14:17:08 +0530

    fully changed html to react

--- js/main.jsx
+++ js/main.jsx
@@ -1,6 +1,35 @@
 
 ReactDOM.render(
-
-  <h1>Hello, all!</h1>,
+  <div className="react-container">
+    <nav className="navbar navbar-default navbar-static-top navbar-inverse">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a href="#" className="navbar-brand logo">SPORT<span className="mblue">AGG</span></a>
+        </div>
+        <div className="collapse navbar-collapse custom">
+          <ul className="nav navbar-nav navbar-right">
+            <li className="active"><a href="#">HOME</a></li>
+            <li><a href="#">SPORTS</a></li>
+            <li><a href="#">EVENTS</a></li>
+            <li><a href="#">GALLERY</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">CONTACT US</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="container-fluid">
+      <div style={{float: "right"}}>
+        <a href="/auth"><button className="btn btn-primary">Login</button></a>
+        <a href="/auth/logout"><button className="btn btn-primary">Logout</button></a>
+      </div>
+      <h1>Hello !</h1>
+      <h1>Login using the button</h1>
+      <div className="image-holder">
+        <img src="/images/screenshot_sportagg_main.png" className="image-overlay"></img>
+      </div>
+    </div>
+    <h1>hi</h1>
+  </div>,
   document.getElementById('example')
 );
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -1,10 +1,139 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
 ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, all!'
+  "div",
+  { className: "react-container" },
+  React.createElement(
+    "nav",
+    { className: "navbar navbar-default navbar-static-top navbar-inverse" },
+    React.createElement(
+      "div",
+      { className: "container-fluid" },
+      React.createElement(
+        "div",
+        { className: "navbar-header" },
+        React.createElement(
+          "a",
+          { href: "#", className: "navbar-brand logo" },
+          "SPORT",
+          React.createElement(
+            "span",
+            { className: "mblue" },
+            "AGG"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "collapse navbar-collapse custom" },
+        React.createElement(
+          "ul",
+          { className: "nav navbar-nav navbar-right" },
+          React.createElement(
+            "li",
+            { className: "active" },
+            React.createElement(
+              "a",
+              { href: "#" },
+              "HOME"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              "a",
+              { href: "#" },
+              "SPORTS"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              "a",
+              { href: "#" },
+              "EVENTS"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              "a",
+              { href: "#" },
+              "GALLERY"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              "a",
+              { href: "#" },
+              "ABOUT US"
+            )
+          ),
+          React.createElement(
+            "li",
+            null,
+            React.createElement(
+              "a",
+              { href: "#" },
+              "CONTACT US"
+            )
+          )
+        )
+      )
+    )
+  ),
+  React.createElement(
+    "div",
+    { className: "container-fluid" },
+    React.createElement(
+      "div",
+      { style: { float: "right" } },
+      React.createElement(
+        "a",
+        { href: "/auth" },
+        React.createElement(
+          "button",
+          { className: "btn btn-primary" },
+          "Login"
+        )
+      ),
+      React.createElement(
+        "a",
+        { href: "/auth/logout" },
+        React.createElement(
+          "button",
+          { className: "btn btn-primary" },
+          "Logout"
+        )
+      )
+    ),
+    React.createElement(
+      "h1",
+      null,
+      "Hello !"
+    ),
+    React.createElement(
+      "h1",
+      null,
+      "Login using the button"
+    ),
+    React.createElement(
+      "div",
+      { className: "image-holder" },
+      React.createElement("img", { src: "/images/screenshot_sportagg_main.png", className: "image-overlay" })
+    )
+  ),
+  React.createElement(
+    "h1",
+    null,
+    "hi"
+  )
 ), document.getElementById('example'));
 
 },{}]},{},[1]);
