0d7e1f4f6683f94b623f4b82fa38ab7e02d64281
vignesh-m <vicky.mano@gmail.com>
2016-01-05 12:27:11 +0530

    added react dependencies

---
+++ js/components/maincontainer.jsx
@@ -0,0 +1,11 @@
+class _MainSection {
+    render() {
+        return (
+            <div>
+                <h1>Example of React with es6 and browserify</h1>
+                <Body />
+            </div>
+        );
+    }
+}
+export const MainSection = React.createClass(_MainSection.prototype);
---
+++ js/main.jsx
@@ -0,0 +1,6 @@
+
+ReactDOM.render(
+
+  <h1>Hello, all!</h1>,
+  document.getElementById('example')
+);
