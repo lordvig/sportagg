8d96faa25ca9a981f983d3d829efbbbeb08e9574
vignesh-m <vicky.mano@gmail.com>
2016-01-08 15:52:15 +0530

    moved auth to db

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,54 @@ var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
-var user = {username:'vignesh'};
+var db_url = 'postgres://ueyntqoaryqfki:IETq0kF0W4pF5xuRge89Uos3Yn@ec2-54-83-52-71.compute-1.amazonaws.com:5432/d9c0dlkf08iie6?ssl=true';
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    if(username=='vignesh' && password=='pass')
-      return done(null,user);
-    else
-      return done(null,false,{message:'Incorrect credentials'});
+    pg.connect(db_url,function(err,client,db_done){
+      if(err){
+        // console.log(process.env.DATABASE_URL)
+        console.log(err);
+        return done(null,false,{err:err,message:'DB error'});
+      }
+      client.query('select id,user from users where username=\''+username+'\' and password=\''+password+'\';',function(err,result){
+        db_done();
+        console.log(result.rows);
+        if(err){
+          return done(null,false,{message:'DB error'});
+        }
+        if(result && result.rows.length>0){
+          return done(null,result.rows[0]);
+        } else {
+          return done(null,false,{err:err,message:'Incorrect credentials'});
+        }
+      });
+    });
   }
 ));
 passport.serializeUser(function(user, done) {
-  done(null, 1);
+  done(null, user.id);
 });
 passport.deserializeUser(function(id, done) {
-  if(id==1)
-    return done(null,user);
-  return done(true,false);
+  pg.connect(db_url,function(err,client,db_done){
+    if(err){
+      console.log(err);
+      return done(true,false);
+    }
+    client.query('select id,username from users where id=\''+id+'\';', function(err,result){
+      db_done();
+      console.log(result.rows);
+      if(err){
+        return done(true,false);
+      }
+      if(result && result.rows.length>0){
+        return done(null,result.rows[0]);
+      } else {
+        return done(true,false);
+      }
+    });
+  });
 });
 router.post('/',passport.authenticate('local',{
   successRedirect: '/',
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   if(req.user){
-    return res.render('index', { user: req.user || {} });
+    return res.render('index', { user: req.user });
   }
   res.render('index', { info: 'Express' });
 });
