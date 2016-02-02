3d2b28c2901727d1df813c9cdf0c9532b5837996
vignesh-m <vicky.mano@gmail.com>
2016-02-02 20:13:00 +0530

    added some card layout

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-mongoose.connect(config.mongo_url);
+// mongoose.connect(config.mongo_url);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
