6489c18c65a2859853e1c692bff33704ca350177
vignesh-m <vicky.mano@gmail.com>
2016-02-03 06:51:53 +0530

    added sport angular data

--- app/components/sports/sport.controller.js
+++ app/components/sports/sport.controller.js
@@ -1,4 +1,18 @@
 angular.module('sportagg')
 .controller('SportController',['$scope','$routeParams',function($scope, $routeParams){
   $scope.sportName=$routeParams.sportName;
+  $scope.grounds=[
+    {
+      name: "ABCD grounds",
+      location: "Nungambakkam"
+    },
+    {
+      name: "Linguswamy Stadium",
+      location: "Meenambakkam"
+    },
+    {
+      name: "YMCA grounds",
+      location: "Kilpauk"
+    }
+  ]
 }]);
